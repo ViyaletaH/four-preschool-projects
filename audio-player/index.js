557079ae442ae5a92d8a853e2eb8920b0ac4031c
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2022-02-06 01:39:51 +0100

    feat: add song change function

--- audio-player/index.js
+++ audio-player/index.js
@@ -6,6 +6,38 @@ const audio = document.querySelector('audio');
 const progress = document.querySelector('input[type="range"]');
 const currentTime = document.querySelector('.currentTime');
 const durationTime = document.querySelector('.durationTime');
+const track = document.querySelector('.artistSong');
+const next = document.getElementById('next');
+const back = document.getElementById('back');
+const player = document.querySelector('.player-functional');
+const main = document.querySelector('.player-main');
+const current = document.querySelector('.currentTime');
+const duration = document.querySelector('.durationTime');
+
+next.addEventListener('click', () => {
+    document.getElementById("gir-bg").src="assets/img/lotl.jpg";
+    document.getElementById("audio").src="assets/audio/Haythor.mp3";
+    artist.textContent = 'LOTL';
+    songName.textContent = 'Haythor';
+    track.classList.add('lotl');
+    player.classList.add('lotl');
+    main.classList.add('lotl');
+    current.classList.add('lotl');
+    duration.classList.add('lotl');
+})
+
+
+back.addEventListener('click', () => {
+    document.getElementById("gir-bg").src="assets/img/girugamesh.jpg";
+    document.getElementById("audio").src="assets/audio/Evolution.mp3";
+    artist.textContent = 'Girugamesh';
+    songName.textContent = 'Evolution';
+    track.classList.remove('lotl');
+    player.classList.remove('lotl');
+    main.classList.remove('lotl');
+    current.classList.remove('lotl');
+    duration.classList.remove('lotl');
+})
 
 function playOrStop() {
     if (count == 0) {
@@ -24,6 +56,12 @@ function playOrStop() {
 }
 
 play.addEventListener('click', playOrStop);
+play.addEventListener('click', () => {
+    track.classList.toggle('anime') 
+})
+play.addEventListener('click', () => {
+    main.classList.toggle('active')
+})
 
 
 progress.addEventListener('change', () => {
@@ -59,6 +97,7 @@ audio.addEventListener('timeupdate', () => {
 audio.addEventListener('ended', () =>  {
     progress.value  = 0;
     count = 0;
+    track.classList.remove('anime');
     document.getElementById("playStop").src="assets/png/play.png";
 })
 
