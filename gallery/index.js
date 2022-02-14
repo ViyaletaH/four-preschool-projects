93bfa18d501c394391bc61cb78711197a2382d09
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2022-02-14 04:27:31 +0100

    feat: add mao force function

--- gallery/index.js
+++ gallery/index.js
@@ -1,8 +1,12 @@
-const basicUrl =  "https://api.unsplash.com/search/photos?query=mao&client_id=6adFL1um8JXRIrgsfChxvwqAc_f1MVYZKe5lOBtuSek";
+const basicUrl =
+  "https://api.unsplash.com/search/photos?query=beijing&client_id=6adFL1um8JXRIrgsfChxvwqAc_f1MVYZKe5lOBtuSek";
+const china = "https://api.unsplash.com/search/photos?query=china&client_id=6adFL1um8JXRIrgsfChxvwqAc_f1MVYZKe5lOBtuSek";
 const search = document.querySelector("form");
 const searcher = document.querySelector(".searcherItself");
 const glass = document.querySelector(".glass");
-const close = document.querySelector('.close');
+const close = document.querySelector(".close");
+const mao = document.querySelector(".mao");
+const back = document.querySelector('.back');
 
 getData(basicUrl);
 async function getData(url) {
@@ -37,18 +41,33 @@ search.addEventListener("submit", (el) => {
     searcher.value +
     "&client_id=6adFL1um8JXRIrgsfChxvwqAc_f1MVYZKe5lOBtuSek";
   getData(searchContent);
-  close.classList.add('active');
+  close.classList.add("active");
+  if (searcher.value == 'america' || searcher.value == 'America' ||  searcher.value == 'AMERICA' || searcher.value == 'New York' || searcher.value == 'new york' || searcher.value == "'america'") {
+    mao.classList.add('force');
+    back.classList.add('enable');
+    }
   
 });
 
+back.addEventListener('click', () => {
+    if (mao.classList.contains('force'))  {
+        getData(china);
+        mao.classList.remove('force');
+        back.classList.remove('enable');
+        const music = document.createElement("div");
+        music.classList.add("music");
+        music.innerHTML = `<embed src="./assets/music.mp3" autostart="true">`;
+        back.appendChild(music);
+    }
+})
+
 glass.addEventListener("click", () => {
-  el.preventDefault();
-  
+
   getData(searchContent);
-  close.classList.add('active');
+  close.classList.add("active");
 });
 
 close.addEventListener("click", () => {
-    searcher.value  = "";
-    close.classList.remove('active');
-})
+  searcher.value = "";
+  close.classList.remove("active");
+});
