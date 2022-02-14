7605110180cb972c8b6a060fb20af61304d1ad81
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2022-02-14 22:21:35 +0100

    docs: evaluate my work in index.js

--- gallery/index.js
+++ gallery/index.js
@@ -1,5 +1,5 @@
 const basicUrl =
-  "https://api.unsplash.com/search/photos?query=beijing&client_id=6adFL1um8JXRIrgsfChxvwqAc_f1MVYZKe5lOBtuSek";
+  "https://api.unsplash.com/search/photos?query=shanghai+night&client_id=6adFL1um8JXRIrgsfChxvwqAc_f1MVYZKe5lOBtuSek";
 const china = "https://api.unsplash.com/search/photos?query=china&client_id=6adFL1um8JXRIrgsfChxvwqAc_f1MVYZKe5lOBtuSek";
 const search = document.querySelector("form");
 const searcher = document.querySelector(".searcherItself");
@@ -73,6 +73,23 @@ search.addEventListener("submit", (el) => {
     }
 });
 
+glass.addEventListener("click", () => {
+    const searchContent =
+    "https://api.unsplash.com/search/photos?query=" +
+    searcher.value +
+    "&client_id=6adFL1um8JXRIrgsfChxvwqAc_f1MVYZKe5lOBtuSek";
+  getData(searchContent);
+  close.classList.add("active");
+      if (searcher.value == 'america' || searcher.value == 'America' ||  searcher.value == 'AMERICA' || searcher.value == 'New York' || searcher.value == 'new york' || searcher.value == "'america'") {
+        mao.classList.add('force');
+        back.classList.add('enable');
+        } 
+        else {
+            document.querySelector('.music').innerHTML = '';
+        }
+});
+
+
 back.addEventListener('click', () => {
     if (mao.classList.contains('force'))  {
         getData(china);
@@ -85,13 +102,10 @@ back.addEventListener('click', () => {
     }
 })
 
-glass.addEventListener("click", () => {
-
-  getData(searchContent);
-  close.classList.add("active");
-});
 
 close.addEventListener("click", () => {
   searcher.value = "";
   close.classList.remove("active");
 });
+
+console.log('Вёрстка +10\nна странице есть несколько фото и строка поиска +5-да\nв футере приложения есть ссылка на гитхаб автора приложения, год создания приложения, логотип курса со ссылкой на курс +5-да\nПри загрузке приложения на странице отображаются полученные от API изображения +10-да\nЕсли в поле поиска ввести слово и отправить поисковый запрос, на странице отобразятся изображения соответствующей тематики, если такие данные предоставляет API +10-да\nПоиск +30\nпри открытии приложения курсор находится в поле ввода +5-да\nесть placeholder +5-да\nавтозаполнение поля ввода отключено (нет выпадающего списка с предыдущими запросами) +5-да\nпоисковый запрос можно отправить нажатием клавиши Enter +5-да\nпосле отправки поискового запроса и отображения результатов поиска, поисковый запрос продолжает отображаться в поле ввода +5-да\nв поле ввода есть крестик при клике по которому поисковый запрос из поля ввода удаляется и отображается placeholder +5-да\nОчень высокое качество оформления приложения и/или дополнительный не предусмотренный в задании функционал, улучшающий качество приложения +10-дополнительный функционал\nвысокое качество оформления приложения предполагает собственное оригинальное оформление равное или отличающееся в лучшую сторону по сравнению с демо');
