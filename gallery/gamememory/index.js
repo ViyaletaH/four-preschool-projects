21a104ad048a2d912440889cc4460237f09f4bec
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2022-02-17 23:21:48 +0100

    feat: add entire randomising function

--- gallery/gamememory/index.js
+++ gallery/gamememory/index.js
@@ -10,12 +10,43 @@ buttonStart.addEventListener('click', () => {
 })
 
 const imgCover = document.querySelector('img');
-    const imgUke = document.querySelector('.two');
+const imgUke = document.querySelector('.two');
 
     imgCover.addEventListener('click', () => {
         imgCover.classList.add('open');
         imgUke.classList.add('open');
     })
+
+const numbersArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+numbersArray.sort(()=>Math.random()-0.5);
+const arrayNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+arrayNumbers.sort(()=>Math.random()-0.5);
+
+const objectA = {
+    'assets/img/brenda-min.png': numbersArray[0],
+    'assets/img/couple-min.png': numbersArray[1],
+    'assets/img/jeffrey-min.png': numbersArray[2],
+    'assets/img/ted-min.png': numbersArray[3],
+    'assets/img/slivko-min.png': numbersArray[4],
+    'assets/img/aileen-min.png': numbersArray[5],
+    'assets/img/sada-min.png': numbersArray[6],
+    'assets/img/manson-min.png': numbersArray[7],
+    'assets/img/ed-min.png': numbersArray[8],
+    'assets/img/zodiac-min.png': numbersArray[9],
+}
+
+const objectB = {
+    'assets/img/brenda-min.png': arrayNumbers[0],
+    'assets/img/couple-min.png': arrayNumbers[1],
+    'assets/img/jeffrey-min.png': arrayNumbers[2],
+    'assets/img/ted-min.png': arrayNumbers[3],
+    'assets/img/slivko-min.png': arrayNumbers[4],
+    'assets/img/aileen-min.png': arrayNumbers[5],
+    'assets/img/sada-min.png': arrayNumbers[6],
+    'assets/img/manson-min.png': arrayNumbers[7],
+    'assets/img/ed-min.png': arrayNumbers[8],
+    'assets/img/zodiac-min.png': arrayNumbers[9],
+}
 
 
 // function turningCards(data) {
