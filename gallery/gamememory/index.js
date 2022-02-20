ebc48ec95dcbe4cef2ad0002a7912265a5eaf831
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2022-02-21 00:53:35 +0100

    feat: add reload function

--- gallery/gamememory/index.js
+++ gallery/gamememory/index.js
@@ -1,4 +1,4 @@
-const buttonStart  = document.querySelector('button');
+
 const buttonCont = document.querySelector('.button-container');
 const cards = document.querySelectorAll('.card');
 const gameplay = document.querySelector('.gameplay')
@@ -8,9 +8,11 @@ let turned = false;
 let killerOne, killerTwo;
 let block = false;
 let count = [];
-const reset = document.querySelector('.play-again');
+
 const records = document.querySelector('.records');
 const container =  document.querySelector('.another-buttons-container');
+const buttonStart  = document.querySelector('button');
+
 
 buttonStart.addEventListener('click', () => {
     buttonStart.classList.add('clicked');
@@ -24,69 +26,64 @@ function mix() {
         let random  = Math.floor(Math.random() * 20);
         card.style.order = random;
     })
-}
-
-
-
-function turn() {
-    if (block) return;
-    if (this === killerOne) return;
-    this.classList.add('open'); 
-    if (block) return;
-    if(!turned) {
-        turned = true;
-        killerOne = this;
-
-        return;
-
-    } 
-    
-    turned = false;
-    killerTwo = this;
-    match();
-    
 
 }
 
-function match() {
-    
-    if (killerOne.dataset.killer === killerTwo.dataset.killer) {
-        isMatch();
-    } else {
-        isNotMatch();
+    function turn() {
+        if (block) return;
+        if (this === killerOne) return;
+        this.classList.add('open'); 
+        if (block) return;
+        if(!turned) {
+            turned = true;
+            killerOne = this;
+
+            return;
+
+        } 
+        
+        turned = false;
+        killerTwo = this;
+        match();
+        
+
     }
-}
 
-function isMatch() {
-    killerOne.removeEventListener('click', turn);
-    killerTwo.removeEventListener('click', turn);
-    count.push('match');
-    if (count.length == 10) {
-        gameplay.classList.remove('open');
-        reset.classList.add('open');
-        records.classList.add('open');
-        container.classList.add('open');
+    function match() {
+        
+        if (killerOne.dataset.killer === killerTwo.dataset.killer) {
+            isMatch();
+        } else {
+            isNotMatch();
+        }
     }
-}
 
-function isNotMatch() {
-    block = true;
-    setTimeout(() => {
-        killerOne.classList.remove('open');
-        killerTwo.classList.remove('open');
-        block =  false;
-    }, 1500);
-    
-}
+    function isMatch() {
+        killerOne.removeEventListener('click', turn);
+        killerTwo.removeEventListener('click', turn);
+        count.push('match');
+        if (count.length == 10) {
+            gameplay.classList.remove('open');
+            reset.classList.add('open');
+            count.length = 0;
+            container.classList.add('open');
+        }
+    }
+
+    function isNotMatch() {
+        block = true;
+        setTimeout(() => {
+            killerOne.classList.remove('open');
+            killerTwo.classList.remove('open');
+            block =  false;
+        }, 1500);
+        
+    }
 
 cards.forEach(card => card.addEventListener('click', turn));
-
+const reset = document.querySelector('.play-again');
 reset.addEventListener('click', () => {
-    reset.classList.remove('open');
-    records.classList.remove('open');
-    container.classList.remove('open');
-    setTimeout(() =>gameplay.classList.add('open'), 3000);
-    mix();
+    location.reload();
+       
 })
-
 
