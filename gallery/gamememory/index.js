45cd99db2a8503eac6d4a157f93523abdd37f78a
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2022-02-20 16:52:37 +0100

    feat: add turning function

--- gallery/gamememory/index.js
+++ gallery/gamememory/index.js
@@ -2,9 +2,8 @@ const buttonStart  = document.querySelector('button');
 const buttonCont = document.querySelector('.button-container');
 const cards = document.querySelectorAll('.card');
 const gameplay = document.querySelector('.gameplay')
-// const card = document.querySelectorAll('.card');
-// const arrayC = [card];
-// console.log(arrayC);
+const killers = document.querySelectorAll('.killer');
+const covers = document.querySelectorAll('.cover');
 
 
 buttonStart.addEventListener('click', () => {
@@ -21,31 +20,250 @@ function mix() {
     })
 }
 
-const imgCover = document.querySelector('img');
-const imgBr = document.querySelector('.brendamin');  ;
-const imgCou = document.querySelector('.couplemin');
-const imgJef = document.querySelector('.jeffreymin');
-const imgTed = document.querySelector('.tedmin');
-const imgSliv = document.querySelector('.slivkomin');
-const imgAil = document.querySelector('.aileenmin');
-const imgSada = document.querySelector('.sadamin');
-const imgMan = document.querySelector('.mansonmin');
-const imgEd = document.querySelector('.edmin');
-const imgZod = document.querySelector('.zodiacmin');
-    imgCover.addEventListener('click', () => {
-        imgCover.classList.add('open');
-        imgBr.classList.add('open');
-        imgCou.classList.add('open');
-        imgJef.classList.add('open');
-        imgTed.classList.add('open');
-        imgSliv.classList.add('open');
-        imgAil.classList.add('open');
-        imgSada.classList.add('open');
-        imgMan.classList.add('open');
-        imgEd.classList.add('open');
-        imgZod.classList.add('open');
-    })
+let turned = false;
+let killerOne, killerTwo;
+let block = false;
 
+function turn() {
+    this.classList.toggle('open'); 
+    if (block) return;
+    if(!turned) {
+        turned = true;
+        killerOne = this;
+    } else {
+        turned = false;
+        killerTwo = this;
+    }
+
+}
+
+cards.forEach(card => card.addEventListener('click', turn));
+
+
+
+
+// covers.forEach(cover => cover.addEventListener('click', openTwo));
+// killers.forEach(killer => killer.classList.add('open'));
+// function open() {
+//     this.classList.add('open');
+// }
+// killers.forEach(killer => killer.addEventListener('click', open));
+// const imgCover1 = document.querySelector('.img1');
+// const imgCover2 = document.querySelector('.img2');
+// const imgCover3 = document.querySelector('.img3');
+// const imgCover4 = document.querySelector('.img4');
+// const imgCover5 = document.querySelector('.img5');
+// const imgCover6 = document.querySelector('.img6');
+// const imgCover7 = document.querySelector('.img7');
+// const imgCover8 = document.querySelector('.img8');
+// const imgCover9 = document.querySelector('.img9');
+// const imgCover10 = document.querySelector('.img10');
+// const imgCover11 = document.querySelector('.img11');
+// const imgCover12 = document.querySelector('.img12');
+// const imgCover13 = document.querySelector('.img13');
+// const imgCover14 = document.querySelector('.img14');
+// const imgCover15 = document.querySelector('.img15');
+// const imgCover16 = document.querySelector('.img16');
+// const imgCover17 = document.querySelector('.img17');
+// const imgCover18 = document.querySelector('.img18');
+// const imgCover19 = document.querySelector('.img19');
+// const imgCover20 = document.querySelector('.img20');
+
+// const img1 = document.getElementById('1');
+// const img2 = document.getElementById('2'); 
+// const img3 = document.getElementById('3'); 
+// const img4 = document.getElementById('4'); 
+// const img5 = document.getElementById('5');
+// const img6 = document.getElementById('6'); 
+// const img7 = document.getElementById('7'); 
+// const img8 = document.getElementById('8'); 
+// const img9 = document.getElementById('9');
+// const img10 = document.getElementById('10'); 
+// const img11 = document.getElementById('11'); 
+// const img12 = document.getElementById('12'); 
+// const img13 = document.getElementById('13');
+// const img14 = document.getElementById('14'); 
+// const img15 = document.getElementById('15'); 
+// const img16 = document.getElementById('16'); 
+// const img17 = document.getElementById('17');
+// const img18 = document.getElementById('18'); 
+// const img19 = document.getElementById('19'); 
+// const img20 = document.getElementById('20'); 
+
+// let firstCard = [];
+// let secondCard = []; 
+
+// imgCover1.addEventListener('click', () => {
+//     imgCover1.classList.add('open');
+//     img1.classList.add('open');
+//     // firstCard.push(img1);
+//     // firstCard.length === 1 && secondCard.length === 1 ? match() : console.log('not enough');
+// })
+
+// imgCover2.addEventListener('click', () => {
+//     imgCover2.classList.add('open');
+//     img2.classList.add('open');
+        
+// })
+
+// imgCover3.addEventListener('click', () => {
+//     imgCover3.classList.add('open');
+//     img3.classList.add('open');
+            
+// })
+
+// imgCover4.addEventListener('click', () => {
+//     imgCover4.classList.add('open');
+//     img4.classList.add('open');
+            
+// })
+
+// imgCover5.addEventListener('click', () => {
+//     imgCover5.classList.add('open');
+//     img5.classList.add('open');
+        
+// })
+
+// imgCover6.addEventListener('click', () => {
+//     imgCover6.classList.add('open');
+//     img6.classList.add('open');
+ 
+// })
+
+// imgCover7.addEventListener('click', () => {
+//     imgCover7.classList.add('open');
+//     img7.classList.add('open');
+        
+// })
+
+// imgCover8.addEventListener('click', () => {
+//     imgCover8.classList.add('open');
+//     img8.classList.add('open');
+       
+// })
+
+// imgCover9.addEventListener('click', () => {
+//     imgCover9.classList.add('open');
+//     img9.classList.add('open');
+      
+// })
+
+// imgCover10.addEventListener('click', () => {
+//     imgCover10.classList.add('open');
+//     img10.classList.add('open');
+//     secondCard.push(img10);
+//     firstCard.length === 1 && secondCard.length === 1 ? match() : console.log('not enough');
+// })
+
+// imgCover11.addEventListener('click', () => {
+//     imgCover11.classList.add('open');
+//     img11.classList.add('open');
+       
+// })
+
+// imgCover12.addEventListener('click', () => {
+//     imgCover12.classList.add('open');
+//     img12.classList.add('open');
+        
+// })
+
+// imgCover13.addEventListener('click', () => {
+//     imgCover13.classList.add('open');
+//     img13.classList.add('open');
+  
+// })
+
+// imgCover14.addEventListener('click', () => {
+//     imgCover14.classList.add('open');
+//     img14.classList.add('open');
+        
+// })
+
+// imgCover15.addEventListener('click', () => {
+//     imgCover15.classList.add('open');
+//     img15.classList.add('open');
+       
+// })
+
+// imgCover16.addEventListener('click', () => {
+//     imgCover16.classList.add('open');
+//     img16.classList.add('open');
+       
+// })
+
+// imgCover17.addEventListener('click', () => {
+//     imgCover17.classList.add('open');
+//     img17.classList.add('open');
+       
+// })
+
+// imgCover18.addEventListener('click', () => {
+//     imgCover18.classList.add('open');
+//     img18.classList.add('open');
+       
+// })
+
+// imgCover19.addEventListener('click', () => {
+//     imgCover19.classList.add('open');
+//     img19.classList.add('open');
+        
+// })
+
+// imgCover20.addEventListener('click', () => {
+//     imgCover20.classList.add('open');
+//     img20.classList.add('open');
+//     firstCard.push(img20);
+//     firstCard.length === 1 && secondCard.length === 1 ? match() : console.log('not enough');
+// })
+
+// function match() {
+//     if (firstCard === secondCard) {
+//         console.log('match');
+//         console.log(firstCard, secondCard);
+//     } else {
+//         console.log('none');
+//     }
+// }
+
+
+// let turned = false;
+// let cardOne, cardTwo;
+
+// function match() {
+//     if (!turned) {
+//         turned = true;
+//         cardOne  = this;
+//     } else {
+//         turned = false;
+//         cardTwo = this;
+
+//         if (cardOne.dataset.killer === cardTwo.dataset.killer) {
+//             console.log('match');
+//         }
+//     }
+
+
+// }
+
+// function match(array) {
+//     array = chosenCards;
+//     var test  =  array[1].toLowerCase();
+//     var target  = array[0].toLowerCase();
+//     for (var i = 0; i < test.length; i++) {
+//         if (target.indexOf(test[i]) < 0)
+//         console.log('none');
+//         }
+//         console.log('true');
+    
+    
+   
+// }
+
+// console.log(chosenCards);
+
+// const card = document.querySelectorAll('.card');
+// const arrayC = [card];
+// console.log(arrayC);
 // const arrayNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
 // function reArrange(){
 // arrayNumbers.sort(()=>Math.random()-0.5);
